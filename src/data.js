b7c35485760352a097d2403fd9fcf7321c9a74e3
dGrammatiko <d.grammatiko@gmail.com>
2019-08-29 22:03:59 +0200

    Update data.js

--- src/data.js
+++ src/data.js
@@ -8,7 +8,6 @@ const dataControllerPhp = `<?php
  * @copyright  Copyright (C) 2016 Michael Babker. All rights reserved.
  * @license    GNU/GPL - http://www.gnu.org/copyleft/gpl.html
  */
-
 defined('_JEXEC') or die;
 
 use Joomla\\CMS\\Factory;
